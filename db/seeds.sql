7f0b205e39727c35469079737438026952c85a1e
TeachY0u <marianoteach@rocketmail.com>
2021-08-31 22:20:49 -0700

    adding to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,23 @@
-INSERT INTO department (first_name, last_name, enrolled)
-VALUES ("Elliot", "Smith", false),
-       ("Amira", "Afzal", true),
-       ("Christoper", "Lee", true),
-       ("Verónica", "Rodriguez", false),
-       ("Igor", "Stein", true);
+INSERT INTO department (name)
+VALUES ('Engineering')
+
+INSERT INTO department (name)
+VALUES ('Finance')
+
+INSERT INTO department (name)
+VALUES ('Legal')
+
+INSERT INTO department (name)
+VALUES ('Sales')
+
+INSERT INTO role (title, salary, department_id)
+VALUE ('Engineer Manager', 800000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUE ('Marketing Manager', 100000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUE ('Lawyer', 1000000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUE ('Sales Manager', 200000, 4);
