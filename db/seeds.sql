660a824bf53f2517e20f35122a1f4937f60932af
TeachY0u <marianoteach@rocketmail.com>
2021-08-31 21:21:42 -0700

    adding files and creating table

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (first_name, last_name, enrolled)
+VALUES ("Elliot", "Smith", false),
+       ("Amira", "Afzal", true),
+       ("Christoper", "Lee", true),
+       ("Verónica", "Rodriguez", false),
+       ("Igor", "Stein", true);
